41702fcc971baaf172870d80f2af56de9c0cc839
Egor <sparkjetstudiost@gmail.com>
2020-02-20 20:00:41 +0300

    the name argument in path() is changed to autors instead of autor

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('books/', views.books, name='books'),
 	path('books/<int:pk>', views.particular_book, name='particular_book'),
-	path('autors/', views.autors, name='autor'),
+	path('autors/', views.autors, name='autors'),
 ]
