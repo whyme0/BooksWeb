0df3a40620c2a4a77c6d7e8adfce28c0f5fd0874
Egor <sparkjetstudiost@gmail.com>
2020-02-20 18:42:23 +0300

    Add new url 'book/<int:pk>' to urlpatterns

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -6,5 +6,6 @@ app_name = 'bookapp'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('books/', views.books, name='books'),
+	path('books/<int:pk>', views.particular_book, name='particular_book'),
 	path('autors/', views.autors, name='autor'),
 ]
