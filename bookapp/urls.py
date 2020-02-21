3a69d554cb30603260df30fef41b258be7159366
Egor <sparkjetstudiost@gmail.com>
2020-02-22 00:22:41 +0300

    improved urls

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('books/', views.books, name='books'),
 	path('books/<int:pk>', views.particular_book, name='particular_book'),
-	path('autors/', views.autors, name='autors'),
+	path('authors/', views.authors, name='authors'),
+	path('authors/<int:pk>', views.particular_author, name='particular_author')
 ]
