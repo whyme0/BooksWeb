3de992e81148263357e940c71baec360b350a80a
Egor <sparkjetstudiost@gmail.com>
2020-03-07 20:55:15 +0300

    added login and logout url

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'authapp'
 urlpatterns = [
 	path('registration/', views.registration, name='registration'),
-	path('', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += [
+	path('login/',
+		 auth_views.LoginView.as_view(template_name='authapp/login_form.html'),
+		 name='login')
+]
+urlpatterns += [
+	path('logout/',
+		 auth_views.LogoutView.as_view(template_name='authapp/logout.html'),
+		 name='logout')
 ]
