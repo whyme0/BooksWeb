bdf9fc9d2ddec5be5282925d6b7f5abb8a4e179d
Egor <sparkjetstudiost@gmail.com>
2020-03-08 22:22:57 +0300

    only registration form

--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm, UserLoginForm
+from .forms import UserRegistrationForm
 from django.contrib import messages
 
 def registration(request):
