db866d6f6499f1c32185811eacad4fa47358223c
Egor <sparkjetstudiost@gmail.com>
2020-03-12 15:51:05 +0300

    now books list view and authors list view works as classes

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'bookapp'
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('books/', views.books, name='books'),
+	path('books/', views.BooksView.as_view(), name='books'),
 	path('books/<int:pk>', views.particular_book, name='particular_book'),
-	path('authors/', views.authors, name='authors'),
+	path('authors/', views.AuthorsView.as_view(), name='authors'),
 	path('authors/<int:pk>', views.particular_author, name='particular_author'),
 	path('profile/', views.profile, name='profile'),
 ]
