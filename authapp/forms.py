33bcbf97715ff86259d3ae55019a8101d0273ca6
Egor <sparkjetstudiost@gmail.com>
2020-03-15 13:46:49 +0300

    new custom login form

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 class UserRegistrationForm(UserCreationForm):
 	email = forms.EmailField()
@@ -15,3 +16,11 @@ class UserRegistrationForm(UserCreationForm):
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 		unique_together = ['email']
+
+
+class LoginForm(AuthenticationForm):
+	def __init__(self, *args, **kwargs):
+		super(LoginForm, self).__init__(*args, **kwargs)
+
+		self.fields['username'].widget.attrs.update({'placeholder': 'Ваш логин'})
+		self.fields['password'].widget.attrs.update({'placeholder': 'Ваш пароль'})
