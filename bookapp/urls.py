f31e7ecd8e70cd26c74c97fc23bd37ddd95a69e6
Egor <sparkjetstudiost@gmail.com>
2020-03-19 13:42:29 +0300

    HomePage.as_view insteand just index

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 app_name = 'bookapp'
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('', views.HomePage.as_view(), name='index'),
 	path('books/', views.BooksView.as_view(), name='books'),
 	path('books/<int:pk>', views.particular_book, name='particular_book'),
-	path('authors/', views.AuthorsView.as_view(), name='authors'),
+	path('authors/', views.AuthorsView.as_view(), name='authors'),	
 	path('authors/<int:pk>', views.particular_author, name='particular_author'),
 	path('profile/', views.profile, name='profile'),
 ]
