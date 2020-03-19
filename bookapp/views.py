58bdce7f5fd23b4d659daf638c30e52c21cd9c93
Egor <sparkjetstudiost@gmail.com>
2020-03-19 13:41:19 +0300

    index function view changed to HomePage class-based view, added search system for books and authors

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,21 +1,43 @@
 from django.views.generic.list import ListView
+from django.views.generic import TemplateView
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
 from .models import Autor, Book
 
 import datetime
 
-def index(request):
-	return render(request, 'bookapp/index.html')
+class HomePage(TemplateView):
+	template_name = 'bookapp/index.html'
 
 
 class BooksView(ListView):
 	model = Book
 	paginate_by = 1
-	context_object_name = 'books'
+	context_object_name = 'books_list'
 	template_name = 'bookapp/books.html'
-	ordering = ['book_year']
+
+	def get_queryset(self):
+		search_context = self.request.GET.get('search', None)
+
+		books = Book.objects.order_by('book_year')
+		if search_context:
+			# сейчас фильтр не учитывает кириллицу, поскольку используется sqlite
+			books = Book.objects.filter(
+				Q(book_name__icontains=search_context) |
+				Q(book_description__icontains=search_context)
+			)
+
+		return books
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		if search_context := self.request.GET.get('search', None) :
+			context['search_context'] = search_context
+
+		return context
+
 
 
 def particular_book(request, pk):
@@ -36,7 +58,23 @@ class AuthorsView(ListView):
 	paginate_by = 1
 	context_object_name = 'authors'
 	template_name = 'bookapp/authors.html'
-	ordering = ['author_full_name']
+
+	def get_queryset(self):
+		search_context = self.request.GET.get('search', None)
+
+		authors = Autor.objects.order_by('author_full_name')
+		if search_context:
+			authors = Autor.objects.filter(author_full_name=search_context)
+
+		return authors
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		if search_context := self.request.GET.get('search', None):
+			context['search_context'] = search_context
+
+		return context
 
 
 def particular_author(request, pk):
