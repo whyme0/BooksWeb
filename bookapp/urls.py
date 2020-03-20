42498f2edf3ec86c199b7263588c47aeed48d5e4
Egor <sparkjetstudiost@gmail.com>
2020-03-20 23:01:15 +0300

    changed function views to class views

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -6,8 +6,8 @@ app_name = 'bookapp'
 urlpatterns = [
 	path('', views.HomePage.as_view(), name='index'),
 	path('books/', views.BooksView.as_view(), name='books'),
-	path('books/<int:pk>', views.particular_book, name='particular_book'),
+	path('books/<int:pk>', views.ParticularBook.as_view(), name='particular_book'),
 	path('authors/', views.AuthorsView.as_view(), name='authors'),	
-	path('authors/<int:pk>', views.particular_author, name='particular_author'),
-	path('profile/', views.profile, name='profile'),
+	path('authors/<int:pk>', views.ParticularAuthor.as_view(), name='particular_author'),
+	path('profile/', views.ProfilePage.as_view(), name='profile'),
 ]
