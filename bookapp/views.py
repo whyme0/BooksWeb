89753399f8324b63533d09fa2444d5e1dae9ec59
Egor <sparkjetstudiost@gmail.com>
2020-03-20 23:04:09 +0300

    now all views implemented as class-based

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 from django.core.paginator import Paginator
@@ -39,18 +40,18 @@ class BooksView(ListView):
 		return context
 
 
+class ParticularBook(DetailView):
+	template_name = 'bookapp/book_info.html'
+	context_object_name = 'book'
 
-def particular_book(request, pk):
-	book = get_object_or_404(Book, pk=pk)
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['book_author'] = Autor.objects.get(author_full_name=self.get_object().book_autor)
+		
+		return context
 
-	# we need to get author of book for further transfer primary key to <a> href
-	# attribute in book_info.html page at 14 line 
-	book_author = Autor.objects.filter(author_full_name=book.book_autor_info).get()
-	context = {
-		'book': book,
-		'book_author': book_author
-	}
-	return render(request, 'bookapp/book_info.html', context)
+	def get_object(self):
+		return get_object_or_404(Book, pk=self.kwargs.get('pk'))
 
 
 class AuthorsView(ListView):
@@ -77,16 +78,19 @@ class AuthorsView(ListView):
 		return context
 
 
-def particular_author(request, pk):
-	author = get_object_or_404(Autor, pk=pk)
-	if author is not None:
-		author_books = Book.objects.filter(book_autor_info=author.author_full_name)
-		context = {
-			'author': author,
-			'author_books': author_books
-		}
-	return render(request, 'bookapp/author_info.html', context)
-	
+class ParticularAuthor(DetailView):
+	template_name = 'bookapp/author_info.html'
+	context_object_name = 'author'
 
-def profile(request):
-	return render(request, 'bookapp/profile.html')
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['author_books'] = Book.objects.filter(book_autor_info=self.get_object().author_full_name)
+
+		return context
+
+	def get_object(self):
+		return get_object_or_404(Autor, pk=self.kwargs.get('pk'))
+
+
+class ProfilePage(TemplateView):
+	template_name = 'bookapp/profile.html'
