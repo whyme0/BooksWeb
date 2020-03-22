855a9df6e81e8741cc3998ea2a9cf3d99d5d8b1f
Egor <sparkjetstudiost@gmail.com>
2020-03-22 15:12:33 +0300

    added comments queryset to particular book and author, and endless pagination

--- bookapp/views.py
+++ bookapp/views.py
@@ -3,7 +3,10 @@ from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from commentsapp.models import Comment
+from commentsapp.tools import create_comment
 from .models import Autor, Book
 
 import datetime
@@ -40,18 +43,32 @@ class BooksView(ListView):
 		return context
 
 
-class ParticularBook(DetailView):
+class ParticularBook(ListView, DetailView):
 	template_name = 'bookapp/book_info.html'
-	context_object_name = 'book'
+	paginate_by = 1
+	object_list = Comment.objects.all()
+	object = Book
+
+	def get_queryset(self):
+		comments = Comment.objects.filter(related_object_name=self.get_object().book_name)
+		comments = comments.order_by('comment_date')
+		return comments
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 		context['book_author'] = Autor.objects.get(author_full_name=self.get_object().book_autor)
-		
+		context['comments'] = self.get_queryset()
+		context['book'] = self.get_object()
+		# context['current_link'] = self.request.path
+
 		return context
 
 	def get_object(self):
 		return get_object_or_404(Book, pk=self.kwargs.get('pk'))
+
+	def post(self, request, pk, *args, **kwargs):
+		create_comment(request, self.get_object())
+		return redirect('bookapp:particular_book', pk=pk)
 
 
 class AuthorsView(ListView):
@@ -78,18 +95,32 @@ class AuthorsView(ListView):
 		return context
 
 
-class ParticularAuthor(DetailView):
+class ParticularAuthor(ListView, DetailView):
 	template_name = 'bookapp/author_info.html'
-	context_object_name = 'author'
+	paginate_by = 1
+	object_list = Comment.objects.all()
+	object = Autor
+
+	def get_queryset(self):
+		comments = Comment.objects.filter(related_object_name=self.get_object().author_full_name)
+		comments = comments.order_by('comment_date')
+
+		return comments
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 		context['author_books'] = Book.objects.filter(book_autor_info=self.get_object().author_full_name)
+		context['comments'] = self.get_queryset()
+		context['author'] = self.get_object()
 
 		return context
 
 	def get_object(self):
 		return get_object_or_404(Autor, pk=self.kwargs.get('pk'))
+
+	def post(self, request, pk, *args, **kwargs):
+		create_comment(request, self.get_object())
+		return redirect('bookapp:particular_author', pk=pk)
 
 
 class ProfilePage(TemplateView):
